90ac004e51ed6792d532533bd41c116e8ad98bbd
samjtwestlake <samjtwestlake@gmail.com>
2020-04-22 16:12:04 +0100

    Update mobile_adjustment.js

--- mobile_adjustment.js
+++ mobile_adjustment.js
@@ -19,11 +19,22 @@ var head = document.getElementsByTagName('HEAD')[0];
         // Append link element to HTML head 
         head.appendChild(link);
 
-		window.addEventListener("orientationchange", function() {
-		    if ((window.orientation ==0) || (window.orientation == 180)) {
-		    	document.documentElement.style.setProperty("--map-window-height", "200px");
-		    } else if ((window.orientation == -90) || (window.orientation == 90))  {
-		    	document.documentElement.style.setProperty("--map-window-height", "300px");
-		    }
-		}, false);
+	window.addEventListener("orientationchange", function() {
+	    if ((window.orientation ==0) || (window.orientation == 180)) {
+	    	document.documentElement.style.setProperty("--map-window-height", "200px");
+	    } else if ((window.orientation == -90) || (window.orientation == 90))  {
+	    	document.documentElement.style.setProperty("--map-window-height", "300px");
+	    }
+	}, false);
+
+        if (document.getElementsByClassName("meetthecommunity")) {
+                image1 = document.getElementById("image1");
+                image2 = document.getElementById("image2");
+                image1.src = "https://7oakswelcomesrefugees.github.io/RefugeeSpeechBubbles_splitA.png"
+                image2.src = "https://7oakswelcomesrefugees.github.io/RefugeeSpeechBubbles_splitB.png"
+
+                image1.parentElement.style.width = "50%";
+                image2.parentElement.style.width = "50%";
+                image2.parentElement.style.display = "block";
+        }
 }
