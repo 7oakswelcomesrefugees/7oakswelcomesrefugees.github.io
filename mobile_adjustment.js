1252cbf0e7346d69f151ecf872bc51cc7f091928
samjtwestlake <samjtwestlake@gmail.com>
2020-04-23 14:24:22 +0100

    Update mobile_adjustment.js

--- mobile_adjustment.js
+++ mobile_adjustment.js
@@ -1,6 +1,6 @@
 /* Script include tag:
 <script type="text/javascript" src="https://7oakswelcomesrefugees.github.io/mobile_adjustment.js"></script>
-*/
+ */
 
 var screen_width = screen.width;
 if (screen_width < 480 ) {
