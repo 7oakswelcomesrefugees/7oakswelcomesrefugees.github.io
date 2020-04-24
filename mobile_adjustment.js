b422e795aae8cdb53584f22c5cd0be6786ec07b5
samjtwestlake <samjtwestlake@gmail.com>
2020-04-24 01:15:39 +0100

    Update mobile_adjustment.js

--- mobile_adjustment.js
+++ mobile_adjustment.js
@@ -18,21 +18,22 @@ if (screen_width < 1000) {
         trustee_id.parentElement.style.marginTop = "-2px";
     }
 
-
-        var head = document.getElementsByTagName('HEAD')[0];  
-  
-        // Create new link Element 
-        var link = document.createElement('link'); 
-  
-        // set the attributes for link element  
-        link.rel = 'stylesheet';  
+        if (screen_width < 480) {
+            var head = document.getElementsByTagName('HEAD')[0];  
       
-        link.type = 'text/css'; 
+            // Create new link Element 
+            var link = document.createElement('link'); 
       
-        link.href = 'https://7oakswelcomesrefugees.github.io/mobile.css';  
-  
-        // Append link element to HTML head 
-        head.appendChild(link);
+            // set the attributes for link element  
+            link.rel = 'stylesheet';  
+          
+            link.type = 'text/css'; 
+          
+            link.href = 'https://7oakswelcomesrefugees.github.io/mobile.css';  
+      
+            // Append link element to HTML head 
+            head.appendChild(link);
+        }
 
 
 
