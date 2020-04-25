6f70aff3d846438b36a96e246f5c6d594f533469
samjtwestlake <samjtwestlake@gmail.com>
2020-04-25 16:37:35 +0100

    Update mobile_adjustment.js

--- mobile_adjustment.js
+++ mobile_adjustment.js
@@ -160,13 +160,32 @@ if (screen_width < 1050) {
         adjustGoogleMap();
 
         window.addEventListener("orientationchange", function() {
-            setTimeout(adjustGoogleMap, adjust_delay); 
-        }, false);   
-        
-        overlays = document.getElementsByClassName("overlay");
+            console.log("rotated");
+            setTimeout(adjustGoogleMap,  adjust_delay); 
+            }, false);    
 
-        while (overlays[0]) {
-            overlays[0].remove();
+        if (screen_width < 480) {
+
+            pic = lock_control.querySelectorAll("img")[0];
+            pic.src = "https://7oakswelcomesrefugees.github.io/GoogleMapsLogo.png";
+
+            sentence = lock_control.querySelectorAll("p")[0];
+            sentence.innerHTML = "Click to load Google map"
+
+            top_overlay = document.getElementById('top_overlay');
+            top_overlay.removeEventListener('click',unlockMap);
+            top_overlay.removeEventListener('click',lockMap);
+            top_overlay.addEventListener('click', loadMap);
+            
+
+
+        } else {
+            overlays = document.getElementsByClassName("overlay");
+
+            while (overlays[0]) {
+                overlays[0].remove()
+            }
+            
         }
     }
 
