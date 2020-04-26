21b5bb8ac5709025f0126036e00f57fe13b03d82
samjtwestlake <samjtwestlake@gmail.com>
2020-04-26 15:04:41 +0100

    Update mobile_adjustment.js

--- mobile_adjustment.js
+++ mobile_adjustment.js
@@ -24,19 +24,19 @@ if (document.getElementsByClassName("donate")[0]) {
         active_embed = "bank_table";
 
 
-        // var css = '* { -webkit-overflow-scrolling: touch; }',
-        // head = document.head || document.getElementsByTagName('head')[0],
-        // style = document.createElement('style');
+        var css = '* { -webkit-overflow-scrolling: touch; }',
+        head = document.head || document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
 
-        // head.appendChild(style);
+        head.appendChild(style);
 
-        // style.type = 'text/css';
-        // if (style.styleSheet){
-        //   // This is required for IE8 and below.
-        //   style.styleSheet.cssText = css;
-        // } else {
-        //   style.appendChild(document.createTextNode(css));
-        // }
+        style.type = 'text/css';
+        if (style.styleSheet){
+          // This is required for IE8 and below.
+          style.styleSheet.cssText = css;
+        } else {
+          style.appendChild(document.createTextNode(css));
+        }
 
         parent_frame = parent.document.getElementsByTagName('iframe')[0];
         parent_frame.scrolling="no";
