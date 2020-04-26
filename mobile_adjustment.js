2d135423e72b5d44d73deeba98e1e518c4531a9b
samjtwestlake <samjtwestlake@gmail.com>
2020-04-26 21:19:14 +0100

    Update mobile_adjustment.js

--- mobile_adjustment.js
+++ mobile_adjustment.js
@@ -56,8 +56,11 @@ if (screen_width < 1050) {
                 table_div.addEventListener("touchmove", function(event) {
                     console.log("captured");
                     els = event.composedPath();
-                    console.log(event.target);
-                    if (event.target.tagName == "TD") { event.stopPropagation(); }
+                    console.log(event.target.tagName);
+                    if (event.target.tagName == "TD") { 
+                        console.log("match")
+                        event.stopPropagation(); 
+                    }
                 }   ,true); 
                 table_div.addEventListener("touchend", hideScrollAlert); 
             } 
