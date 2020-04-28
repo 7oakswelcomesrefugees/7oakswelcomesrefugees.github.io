b6aedf86068aac14fd446a225ab8f30b0ff983b5
samjtwestlake <samjtwestlake@gmail.com>
2020-04-28 19:36:12 +0100

    Update mobile_adjustment.js

--- mobile_adjustment.js
+++ mobile_adjustment.js
@@ -186,9 +186,9 @@ if (screen_width < 1050) {
                     new_title_str = title_str.replace("Newsletter","News")
                     campaigns[i].children[0].innerText = new_title_str;
                     campaigns[i].style.paddingLeft = 0;
-                    campaigns[i].style.fontSize = "14px";
+                    campaigns[i].childNodes[0].style.fontSize = "16px";
+                    campaigns[i].children[0].style.fontSize = "14px";
                     campaigns[i].style.lineHeight = "160%";
-                    campaigns[i].childNodes[0].style.verticalAlign = "bottom";
                 }
                 new_fontsize = "40px";
             }
