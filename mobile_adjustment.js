b74fda53864271d68963ef7c2fd79dd0f20ddd27
samjtwestlake <samjtwestlake@gmail.com>
2020-04-28 22:45:47 +0100

    Update mobile_adjustment.js

--- mobile_adjustment.js
+++ mobile_adjustment.js
@@ -152,10 +152,6 @@ if (screen_width < 1050) {
             title_div = document.getElementById("title_div");
             title_div.style.textAlign = "center";
 
-            
-            
-
-
             screen_width = screen.width;
             if (iOS) {
                 if ((window.orientation == 0) || (window.orientation == 180)) {
