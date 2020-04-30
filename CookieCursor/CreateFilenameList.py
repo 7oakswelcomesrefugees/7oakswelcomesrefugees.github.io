d14ba3f2f0caa21c59c5a227ef2f8b7e39692b53
samjtwestlake <samjtwestlake@gmail.com>
2020-04-30 14:01:14 +0100

    Create CreateFilenameList.py

---
+++ CookieCursor/CreateFilenameList.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 13:51:26 2020
+
+@author: samwestlake
+"""
+
+import os
+import json
+
+filenames=[]
+for filename in os.listdir("/Users/samwestlake/Google Drive/Sevenoaks Refugees/SWR Website Content/SWR Website - Public Backend Files/7oakswelcomesrefugees.github.io/CookieCursor/"):
+    if filename.endswith(".png"): 
+        filenames.append(filename);
+filenames = sorted(filenames)
+print(filenames)
+
+data = {}
+data['filenames']=filenames;
+print(data)
+
+with open('data.txt', 'w') as outfile:
+    json.dump(data, outfile)
